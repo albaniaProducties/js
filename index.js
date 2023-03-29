a4b494d65299d93670a086355245bebef790113d
albaniaProducties <128729035+albaniaProducties@users.noreply.github.com>
2023-03-29 23:03:21 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      body {
+        background-color: lightblue;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>My Beautiful README</h1>
+    <p>This is my README file with a beautiful background color.</p>
+  </body>
+</html>
